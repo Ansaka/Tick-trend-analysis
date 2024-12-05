0c8ecd16c55a4523435345d5b2a9156531a19547
Antonio <antonio.skondras@gmail.com>
2024-12-05 12:25:49 +0200

    feat: added signal visualization, in progress

--- tick-trend-frontend/src/components/StockChart.js
+++ tick-trend-frontend/src/components/StockChart.js
@@ -7,7 +7,8 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
     const [chartData, setChartData] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
-    const [signals, setSignals] = useState([]);
+    const [buySignals, setBuySignals] = useState([]);
+    const [sellSignals, setSellSignals] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -24,15 +25,19 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
                     ema_38: point.ema_38,
                     ema_100: point.ema_100
                 }));
-                const response_signals = response.data.signals.map(point => [
+                // Filter signals to include only "BUY"
+                const response_signals_buy = response.data.signals.filter(point => point.signal === "BUY").map(point => [
                     new Date(point.datetime).getTime(),
                     point.price
-                    //point.signal
-                ]
-                )
+                ]);
+                const response_signals_sell = response.data.signals.filter(point => point.signal === "SELL").map(point => [
+                    new Date(point.datetime).getTime(),
+                    point.price
+                ]);
                 //console.log(data);
                 setChartData(data);
-                setSignals(response_signals)
+                setBuySignals(response_signals_buy);
+                setSellSignals(response_signals_sell);
             } catch (error) {
                 console.error('Error fetching data:', error);
                 setError('Error loading chart data. Please try again.');
@@ -116,6 +121,35 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
                 color: '#ffffff'
             }
         },
+        tooltip: {
+            backgroundColor: 'rgba(45, 45, 45, 0.9)',
+            borderColor: '#444',
+            style: {
+                color: '#ffffff'
+            },
+            shared: true,
+            formatter: function() {
+                let tooltip = `<b>${symbol}</b><br/>Time: ${Highcharts.dateFormat('%H:%M:%S', this.x)}<br/>`;
+                if (this.points) { // Check if this.points is defined
+                    this.points.forEach(point => {
+                        if (point.series.name === symbol) {
+                            tooltip += `Price: ${point.y.toFixed(2)}<br/>`;
+                        } else if (point.series.name === 'EMA 38') {
+                            tooltip += `EMA 38: ${point.y.toFixed(2)}<br/>`;
+                        } else if (point.series.name === 'EMA 100') {
+                            tooltip += `EMA 100: ${point.y.toFixed(2)}<br/>`;
+                        } else if (point.series.name === 'Sell signals') {
+                            tooltip += `Signal: Sell at ${point.y.toFixed(2)}<br/>`;
+                        } else if (point.series.name === 'Buy signals') {
+                            tooltip += `Signal: Buy at ${point.y.toFixed(2)}<br/>`;
+                        }
+                    });
+                } else {
+                    tooltip += 'No data available for this point.<br/>'; // Fallback message
+                }
+                return tooltip;
+            }
+        },
         series: [
             showPrice ? {
                 name: symbol,
@@ -128,17 +162,38 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
                     hover: {
                         lineWidth: 3
                     }
-                }
+                },
+                /*tooltip: {
+                    pointFormat: 'Price: <b>{point.y:.2f}</b><br/>'
+                }*/
             } : null,
             showSignals ? {
-                name: 'Signals',
+                name: 'Sell signals',
                 type: 'scatter',
-                data: signals,
+                data: sellSignals,
                 marker: {
                     enabled: true,
-                    radius: 10,
-                    fillColor: signal => (signal.signal === 'BUY' ? '#00ff00' : '#ff0000')
-                }
+                    color: 'red',
+                    radius: 5,
+                    symbol: 'circle',
+                },
+                /*tooltip: {
+                    pointFormat: 'Signal: <b>{point.signal}</b> at <b>{point.y:.2f}</b><br/>'
+                },*/
+            } : null,
+            showSignals ? {
+                name: 'Buy signals',
+                type: 'scatter',
+                data: buySignals,
+                marker: {
+                    enabled: true,
+                    color: 'green',
+                    radius: 5,
+                    symbol: 'circle',
+                },
+                /*tooltip: {
+                    pointFormat: 'Signal: <b>{point.signal}</b> at <b>{point.y:.2f}</b><br/>'
+                },*/
             } : null,
             showEma38 ? {
                 name: 'EMA 38',
@@ -151,7 +206,10 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
                     hover: {
                         lineWidth: 3
                     }
-                }
+                },
+                /*tooltip: {
+                    pointFormat: 'EMA 38: <b>{point.y:.2f}</b><br/>'
+                }*/
             } : null,
             showEma100 ? {
                 name: 'EMA 100',
@@ -164,7 +222,10 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
                     hover: {
                         lineWidth: 3
                     }
-                }
+                },
+                /*tooltip: {
+                    pointFormat: 'EMA 100: <b>{point.y:.2f}</b><br/>'
+                }*/
             } : null
         ].filter(Boolean),
         xAxis: {
@@ -194,28 +255,12 @@ const StockChart = ({ symbol, startDate, endDate, showPrice, showEma38, showEma1
                 title: {
                     text: null // No title for secondary axis
                 },
-                labels: { enabled: !showPrice }, // Hide labels
+                labels: { style: { color: '#ffffff' }, enabled: !showPrice }, // Hide labels
                 gridLineWidth: '#333', // No grid lines
                 opposite: true, // Keep on the right
-                linkedTo: (showPrice ? 0 : 1), 
+                linkedTo: (showPrice ? 0 : 1),
             }
         ],
-        tooltip: {
-            backgroundColor: 'rgba(45, 45, 45, 0.9)',
-            borderColor: '#444',
-            style: {
-                color: '#ffffff'
-            },
-            split: false,
-            shared: true,
-            formatter: function() {
-                let tooltip = `<b>${symbol}</b><br/>Time: ${Highcharts.dateFormat('%H:%M:%S', this.x)}<br/>Price: ${this.y.toFixed(2)}`;
-                if (this.point.signal) {
-                    tooltip += `<br/>Signal: ${this.point.signal}`;
-                }
-                return tooltip;
-            }
-        },
         navigator: {
             maskFill: 'rgba(0, 255, 255, 0.1)',
             handles: {
